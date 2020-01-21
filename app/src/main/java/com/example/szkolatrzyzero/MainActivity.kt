b228705fc8820925f2a1e26afe79ea8f0147eed0
Natalia Nazaruk <natalia.nazaruk@nordea.com>
2020-01-21 22:00:58 +0100

    Program changed to guessing game

--- app/src/main/java/com/example/szkolatrzyzero/MainActivity.kt
+++ app/src/main/java/com/example/szkolatrzyzero/MainActivity.kt
@@ -14,23 +14,23 @@ class MainActivity : AppCompatActivity() {
 
     fun ocenLiczbe(v: View) {
         text_wyniku.visibility = View.VISIBLE
+
+        val liczbaZgadywana = 47
+
         val twojaLiczba: Int = Integer.parseInt(twoja_liczba.text.toString())
 
         if (twojaLiczba > 100 || twojaLiczba < 1) {
             text_wyniku.text = "Podałeś liczbę spoza zakresu"
         } else {
             when {
-                twojaLiczba <= 10 -> {
-                    text_wyniku.text = "Twoja liczba jest mala"
+                twojaLiczba < liczbaZgadywana -> {
+                    text_wyniku.text = "Ustalona liczba jest większa"
                 }
-                twojaLiczba in 11..40 -> {
-                    text_wyniku.text = "Twoja liczba jest w miare mala"
+                twojaLiczba == liczbaZgadywana -> {
+                    text_wyniku.text = "Yuhu zgadles!"
                 }
-                twojaLiczba in 41..89 -> {
-                    text_wyniku.text = "Twoja liczba jest calkiem duza"
-                }
-                else -> {
-                    text_wyniku.text = "Twoja liczba jest bardzo duza"
+                twojaLiczba > liczbaZgadywana -> {
+                    text_wyniku.text = "Ustalona liczba jest mniejsza"
                 }
             }
         }
