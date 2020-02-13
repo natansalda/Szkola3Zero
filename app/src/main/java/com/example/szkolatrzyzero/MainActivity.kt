b691df620fb5b693cd28a66b26b6c31a0661f12b
Natalia Nazaruk <nazarjazz@wp.pl>
2020-02-13 11:28:36 +0100

    Fix for ocenLiczbe() method

--- app/src/main/java/com/example/szkolatrzyzero/MainActivity.kt
+++ app/src/main/java/com/example/szkolatrzyzero/MainActivity.kt
@@ -13,28 +13,26 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun ocenLiczbe(v: View) {
+        text_wyniku.text = ""
         text_wyniku.visibility = View.VISIBLE
 
         val twojaLiczba: Int = Integer.parseInt(twoja_liczba.text.toString())
 
         if (twojaLiczba > 100 || twojaLiczba < 1) {
             text_wyniku.text = "Podałeś liczbę spoza zakresu"
-            if (twojaLiczba > 100 || twojaLiczba < 1) {
-                text_wyniku.text = "Podałeś liczbę spoza zakresu"
-            } else {
-                when {
-                    twojaLiczba <= 10 -> {
-                        text_wyniku.text = "Twoja liczba jest mala"
-                    }
-                    twojaLiczba in 11..40 -> {
-                        text_wyniku.text = "Twoja liczba jest w miare mala"
-                    }
-                    twojaLiczba in 41..89 -> {
-                        text_wyniku.text = "Twoja liczba jest calkiem duza"
-                    }
-                    else -> {
-                        text_wyniku.text = "Twoja liczba jest bardzo duza"
-                    }
+        } else {
+            when {
+                twojaLiczba <= 10 -> {
+                    text_wyniku.text = "Twoja liczba jest mala"
+                }
+                twojaLiczba in 11..40 -> {
+                    text_wyniku.text = "Twoja liczba jest w miare mala"
+                }
+                twojaLiczba in 41..89 -> {
+                    text_wyniku.text = "Twoja liczba jest calkiem duza"
+                }
+                else -> {
+                    text_wyniku.text = "Twoja liczba jest bardzo duza"
                 }
             }
         }
