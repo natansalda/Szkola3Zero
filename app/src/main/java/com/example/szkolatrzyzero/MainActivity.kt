ce8abfa8ae93d022bc7e81cc317ea1e1e7849ba1
Natalia Nazaruk <nazarjazz@wp.pl>
2020-02-13 11:16:36 +0100

    Added second button, app now has 2 functions

--- app/src/main/java/com/example/szkolatrzyzero/MainActivity.kt
+++ app/src/main/java/com/example/szkolatrzyzero/MainActivity.kt
@@ -15,6 +15,34 @@ class MainActivity : AppCompatActivity() {
     fun ocenLiczbe(v: View) {
         text_wyniku.visibility = View.VISIBLE
 
+        val twojaLiczba: Int = Integer.parseInt(twoja_liczba.text.toString())
+
+        if (twojaLiczba > 100 || twojaLiczba < 1) {
+            text_wyniku.text = "Podałeś liczbę spoza zakresu"
+            if (twojaLiczba > 100 || twojaLiczba < 1) {
+                text_wyniku.text = "Podałeś liczbę spoza zakresu"
+            } else {
+                when {
+                    twojaLiczba <= 10 -> {
+                        text_wyniku.text = "Twoja liczba jest mala"
+                    }
+                    twojaLiczba in 11..40 -> {
+                        text_wyniku.text = "Twoja liczba jest w miare mala"
+                    }
+                    twojaLiczba in 41..89 -> {
+                        text_wyniku.text = "Twoja liczba jest calkiem duza"
+                    }
+                    else -> {
+                        text_wyniku.text = "Twoja liczba jest bardzo duza"
+                    }
+                }
+            }
+        }
+    }
+
+    fun zgadnijLiczbe(v: View) {
+        text_wyniku.visibility = View.VISIBLE
+
         val liczbaZgadywana = 47
 
         val twojaLiczba: Int = Integer.parseInt(twoja_liczba.text.toString())
